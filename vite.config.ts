887d2f7d856c0ed8d0c061bf9da7c7a40e09e97c
nguyenconguan27 <nguyenconguan2k2@gmail.com>
2023-09-14 02:49:11 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/shoppe-clone-ts',
   plugins: [react(), visualizer()],
   server: {
     port: 3000
